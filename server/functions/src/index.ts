acd32f30137d1dd7cebdc4c5e1aa28501f4ac53d
Greg Ennis <greg@innodroid.com>
2022-09-11 20:59:44 -0400

    Update server

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -6,7 +6,7 @@ import { storeScan } from "./scan_storage";
 exports.processScan = functions.https.onCall(async (data, context) => {
   const uid = (context.auth || {}).uid;
   if (!uid) {
-    return Promise.reject(new functions.https.HttpsError('permission-denied', 'Not Authorized'));
+    return Promise.reject(new functions.https.HttpsError('permission-denied', 'Missing or invalid auth token'));
   }
 
   const product = await getProductInfo(functions.config(), data.upc)
